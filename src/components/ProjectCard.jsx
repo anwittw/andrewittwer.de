f655984b52e93990da7f6a828830129331138e49
anwittw <git@andrewittwer.de>
2019-08-12 17:19:41 +0100

    added ProjectCard

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button
+} from "reactstrap";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGitSquare } from "@fortawesome/free-brands-svg-icons";
+import { faPaperPlane } from "@fortawesome/free-regular-svg-icons";
+
+export default function ProjectCard(props) {
+  return (
+    <div>
+      <Card>
+
+      { props.project.img &&  <CardImg top width="80px" src={props.project.img} alt="Card image cap" /> }
+        <CardBody>
+          <CardTitle className="mb-3 font-weight-bold">
+            {props.project.name}
+          </CardTitle>
+          {props.project.subtitle && (
+            <CardSubtitle className="text-muted mb-3">
+              {props.project.subtitle}
+            </CardSubtitle>
+          )}
+          <CardText>{props.project.summary}</CardText>
+          <div className="ProjectCard__Social">
+            {props.project.gitname && (
+              <span className="ProjectCard__Social__Icon">
+                <a
+                  className="ProjectCard__Social__a"
+                  rel="noopener noreferrer"
+                  href={`https://github.com/anwittw/${props.project.gitname}`}
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={faGitSquare} />
+                </a>
+              </span>
+            )}
+            {props.project.url && (
+              <span className="ProjectCard__Social__Icon">
+                <a
+                  className="ProjectCard__Social__a"
+                  rel="noopener noreferrer"
+                  href={`${props.project.url}`}
+                  target="_blank"
+                >
+                  <FontAwesomeIcon icon={faPaperPlane} />
+                </a>
+              </span>
+            )}
+          </div>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import ProjectsContent from "../../content/projects";
+import ProjectCard from '../ProjectCard'
 
-import { Container, Row, Col } from 'reactstrap'
+import { Container, Row, Col } from "reactstrap";
 
 export default function Projects() {
+  console.log(ProjectsContent);
   return (
-    <div className="Projects">
-Projects
+    <div className="Projects p-5">
+    <div class="card-columns">
+      {ProjectsContent.map((project, i) => (
+        <ProjectCard key={i} project={project} />
+      ))}
+      </div>
     </div>
-  )
+  );
 }
