6c41dd2b824f9b14e4b17955a798b9bcae18c1ee
anwittw <git@andrewittwer.de>
2019-08-14 21:43:34 +0100

    added first iteration of 'Tech-filter'

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -18,13 +18,13 @@ export default function Contact() {
   return (
     <div className="Contact p-md-4">
       <Container>
-        <h1 className="mt-5 mb-3">Where you can meet me for coffee</h1>
+        <h1 className="mt-5 mb-3">Here you can meet me for coffee</h1>
         <Row>
           <Col sm="12" md="4">
             <MapMe />
           </Col>
           <Col sm="12" md={{ offset: 1, size: 7 }} className="mt-4">
-            <h3 className="text-muted">Or rather leave me your E-Mail!</h3>
+            <h3 className="text-muted">Or rather leave me your mail here!</h3>
             <ContactForm />
           </Col>
         </Row>
--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,15 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import ProjectsContent from "../../content/projects";
 import ProjectCard from '../ProjectCard'
 
-import { Container, Row, Col } from "reactstrap";
+import { Container, Row, Col, Badge } from "reactstrap";
 
 export default function Projects() {
-  console.log(ProjectsContent);
+
+  const [stateTechs, setstateTechs] = useState({
+    bootstrap: true,
+    reactstrap: true,
+    expressJS: true,
+    ReactJS: true,
+    hbs: true,
+    passport: true,
+    bCrypt: true,
+    strapi: true
+  })
+
+  function handleClick(e) {
+    let element = e.target.id
+    console.log(element)
+    e.preventDefault()
+    setstateTechs( {
+      ...stateTechs,
+      [element]: !stateTechs[element]
+    })
+  }
+  
   return (
     <div className="Projects p-md-4">
     <Container>
     <h1 className="my-5">Handpicked Projects</h1>
+    <pre>{JSON.stringify(stateTechs,null,2)}</pre> <br />
+    <Row>
+    <Col className="d-flex justify-content-around mb-4 flex-wrap" sm="12" md={{size: 8}}>
+    {Object.keys(stateTechs).map(tech => (
+      <Badge className={stateTechs[tech] ? 'Project__Badge__Active' : 'Project__Badge' } id={tech} onClick={handleClick}>{tech}</Badge>
+    ))}
+    </Col>
+    </Row>
     <div className="card-columns">
       {ProjectsContent.map((project, i) => (
         <ProjectCard key={i} project={project} />
