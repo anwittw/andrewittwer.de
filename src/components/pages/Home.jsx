068e6370f312f137bf87c9831802b2f8cac83503
anwittw <git@andrewittwer.de>
2019-11-02 19:31:44 +0000

    tried animations

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col } from "reactstrap";
 import MyDetails from "../MyDetails";
 import MainNavbar from "../MainNavbar";
 import Socials from "../Socials";
 import Contact from "./Contact";
 import Projects from "./Projects";
+
+import { CSSTransition } from "react-transition-group";
 
 export default function Home() {
   const [stateActive, setstateActive] = useState({
@@ -32,6 +34,11 @@ export default function Home() {
       [state]: true
     });
   }
+
+  useEffect(() => {
+    handleClickState("details");
+  }, []);
+
   return (
     <div className="Home">
       <Container className="my-5 Home__Container">
@@ -47,27 +54,48 @@ export default function Home() {
           </Col>
         </Row>
         <div className="global-shadow">
-          <Row>
-            <Col md={{ size: 12 }}>
-              {stateActive.details && (
+          <CSSTransition
+            in={stateActive.details}
+            timeout={400}
+            classNames="detailAni"
+          >
+            <Row>
+              <Col md={{ size: 12 }}>
                 <MyDetails
                   handleClickContactMe={() => handleClickState("contact")}
                   handleClickPortfolio={() => handleClickState("projects")}
                 />
-              )}
-            </Col>
-          </Row>
+              </Col>
+            </Row>
+          </CSSTransition>
           <Row>
             <Col md={{ size: 12 }}>
               <MainNavbar state={stateActive} handleClick={handleClick} />
             </Col>
           </Row>
-          <Row>
-            <Col md={{ size: 12 }}>
-              {stateActive.projects && <Projects />}
-              {stateActive.contact && <Contact />}
-            </Col>
-          </Row>
+          <CSSTransition
+            in={stateActive.projects}
+            timeout={400}
+            classNames="projectAni"
+          >
+            <Row>
+              <Col md={{ size: 12 }}>
+                <Projects />
+              </Col>
+            </Row>
+          </CSSTransition>
+
+          <CSSTransition
+            in={stateActive.contact}
+            timeout={400}
+            classNames="contactAni"
+          >
+            <Row>
+              <Col md={{ size: 12 }}>
+                <Contact />
+              </Col>
+            </Row>
+          </CSSTransition>
         </div>
       </Container>
     </div>
